d70158b53fbc9a35676368dccc4f4d0b77c2e0be
dbizzoze <dbizzoze@slac.stanford.edu>
2024-10-21 14:58:35 -0700

    new fix attempt

--- lume_ace3p/workflow.py
+++ lume_ace3p/workflow.py
@@ -8,7 +8,7 @@ from lume_ace3p.tools import WriteDataTable
 
 class Omega3PWorkflow:
     
-    def __init__(self, workflow_dict, input_dict=None, output_dict=None):
+    def __init__(self, workflow_dict, input_dict, output_dict):
         self.input_dict = input_dict
         self.output_dict = output_dict
         self.cubit_input = workflow_dict.get('cubit_input')
