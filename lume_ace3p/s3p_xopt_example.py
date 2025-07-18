d376b4e42ef1cc425e8248146be3b5743a65ce40
lf006425 <lf006425@gmail.com>
2025-07-18 10:51:05 -0700

    working on xopt example file

---
+++ lume_ace3p/s3p_xopt_example.py
@@ -0,0 +1,45 @@
+from xopt.vocs import VOCS
+from xopt.evaluator import Evaluator
+from xopt.generators.bayesian import ExpectedImprovementGenerator
+from xopt import Xopt
+from lume_ace3p.workflow import S3PWorkflow
+from lume_ace3p.tools import WriteXoptData
+
+#Define workflow dictionary with input files, directory options, etc.
+workflow_dict = {'cubit_input': 'pillbox-rtop.jou',
+                 'ace3p_input': 'pillbox-rtop.s3p',
+                 'ace3p_tasks': 16,
+                 'ace3p_cores': 16,
+                 'ace3p_opts' : '--cpu-bind=cores',
+                 'rfpost_input': 'pillbox-rtop.rfpost',
+                 'workdir': 'lume-ace3p_xopt_workdir'}
+
+#Define variables and function objectives/constraints/observables
+vocs = VOCS(
+    variables={"cav_radius": [95, 105], "ellipticity": [0.5, 1.2]},
+    objectives={"S(0,0)": "MAXIMIZE"}
+)
+
+#Define simulation function for xopt (based on workflow w/ postprocessing)
+def sim_function(input_dict):
+
+    #Create workflow object and run with provided inputs
+    workflow = S3PWorkflow(workflow_dict,input_dict,output_dict)
+    output_data = workflow.run()
+
+    return output_data
+
+#Create Xopt evaluator, generator, and Xopt objects
+evaluator = Evaluator(function=sim_function)
+generator = ExpectedImprovementGenerator(vocs=vocs)
+X = Xopt(evaluator=evaluator, generator=generator, vocs=vocs)
+
+#Run X.random_evaluate() to generate + evaluate a few initial points
+for i in range(5):
+    X.random_evaluate()
+    WriteXoptData('sim_output.txt',X)
+
+#Run optimization for subsequent steps
+for i in range(15):
+    X.step()
+    WriteXoptData('sim_output.txt',X)
