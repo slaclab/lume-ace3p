9c1081d1742d05020b5df3c6789260026c55b4a3
lf006425 <lf006425@gmail.com>
2025-07-21 11:43:22 -0700

    testing xopt run with a yaml file

--- lume_ace3p/run_lume_ace3p.py
+++ lume_ace3p/run_lume_ace3p.py
@@ -99,6 +99,40 @@ if workflow_dict['mode'].lower() == 'parameter_sweep':
     elif workflow_dict['module'].lower() == 'omega3p':
         workflow = Omega3PWorkflow(workflow_dict, input_dict, output_dict)
         workflow.run_sweep()
-    elif workflow_dict['module'].lower() == 'scalar_optimize':
-        workflow = S3PWorkflow(workflow_dict, input_dict)
-        workflow.run_scalar_opt()
+if workflow_dict['mode'].lower() == 'scalar_optimize':
+    if workflow_dict['module'].lower() == 's3p':
+        vocs_dict = lume_ace3p_data.get('vocs_parameters')
+        
+        from xopt.vocs import VOCS
+        from xopt.evaluator import Evaluator
+        from xopt.generators.bayesian import ExpectedImprovementGenerator
+        from xopt import Xopt
+        from lume_ace3p.workflow import S3PWorkflow
+        from lume_ace3p.tools import WriteXoptData
+        
+        #Define variables and function objectives/constraints/observables
+        vocs = VOCS(variables=vocs_dict['variables'], objectives=vocs_dict['objectives'])
+
+        #Define simulation function for xopt (based on workflow w/ postprocessing)
+        def sim_function(input_dict):
+
+            #Create workflow object and run with provided inputs
+            workflow = S3PWorkflow(workflow_dict,input_dict)
+            output_data = workflow.run()
+
+            return output_data
+
+        #Create Xopt evaluator, generator, and Xopt objects
+        evaluator = Evaluator(function=sim_function)
+        generator = ExpectedImprovementGenerator(vocs=vocs)
+        X = Xopt(evaluator=evaluator, generator=generator, vocs=vocs)
+
+        #Run X.random_evaluate() to generate + evaluate a few initial points
+        for i in range(5):
+            X.random_evaluate()
+            WriteXoptData('sim_output.txt',X)
+
+        #Run optimization for subsequent steps
+        for i in range(15):
+            X.step()
+            WriteXoptData('sim_output.txt',X)
