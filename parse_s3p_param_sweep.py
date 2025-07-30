ef50f5ab404edf8f4affeaaf06e3335e61b70ebe
lf006425 <lf006425@gmail.com>
2025-07-30 14:17:03 -0700

    modifying parsing script

--- parse_s3p_param_sweep.py
+++ parse_s3p_param_sweep.py
@@ -1,31 +1,36 @@
 import os
 import numpy as np
 
+if len(sys.argv) == 2:
+    file_path = sys.argv[1]
+else:
+    file_path = filedialog.askopenfilename()
+
 freq = input('Enter the frequency you want to isolate: ')
 S_param = input('Enter the S parameter you want to isolate: ')
-subdirectories = [entry.name for entry in os.scandir('.') if entry.is_dir()]
 
-data_array = {}
 
-#NOTE: only works on two param sweeps!
-for subdir in subdirectories:
-    if subdir.startswith('lume-ace3p_s3p_workdir'):
-        first_half, second_param = subdir.rsplit('_')
-        first_param = float(first_half.rsplit('_')[1])
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+    
+colnames = lines[0].split()
+freq_index = 0
+s_index = 0
+for i in range(len(colnames)):
+    if colnames[i]==S_param:
+        s_index = i
+    elif colnames[i]=='Frequency':
+        freq_index = i
+data = {}
+for l in lines[1:]:
+    linesplit = l.split()
+    if linesplit[freq_index] == freq:
+        key = '('
+        for k in range(freq_index):
+            key += linesplit[k]
+            if k!=freq_index-1:
+                key += ','
+        key += ')'
+    data[key] = linesplit[s_index]
 
-        os.chdir(subdir)
-        os.chdir('s3p_results')
-        with open('Reflection.out') as file:
-            lines = file.readlines()
-        colnames = []
-        s_index = 0
-        for l in lines:
-            if l.startswith('#Frequency'):
-                colnames = l.split()
-                s_index = colnames.index(S_param)
-            if l.startswith(freq):
-                data_array[(float(first_param), float(second_param))] = l.split()[s_index]
-
-        os.chdir('../..')
-        
-print(data_array)
+print(data)
